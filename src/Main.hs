c8cf0dd6856d81901c79108fca4992546b84a5f0
Utku Demir <me@utdemir.com>
2020-07-18 20:11:16 +1200

    revert main

--- src/Main.hs
+++ src/Main.hs
@@ -9,26 +9,27 @@ import Parse
 import System.Environment
 import System.IO
 
-main = do
-  input <- readFile "galaxy.txt"
-  let env = parseAndEvalEnv input
-      r = find (\d -> dLhs d == "galaxy") env
-  hPutStrLn stderr $ "Result: " ++ show r
+-- main = do
+--   input <- readFile "galaxy.txt"
+--   let env = parseAndEvalEnv input
+--       r = find (\d -> dLhs d == "galaxy") env
+--   hPutStrLn stderr $ "Result: " ++ show r
 
--- main :: IO ()
--- main = catch (
---     do
---         args <- getArgs
---         putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
---         request' <- parseRequest ("POST " ++ (args!!0))
---         let request = setRequestBodyLBS (BLU.fromString (args!!1)) request'
---         response <- httpLBS request
---         let statuscode = show (getResponseStatusCode response)
---         case statuscode of
---             "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
---             _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
---     ) handler
---     where
---         handler :: SomeException -> IO ()
---         handler ex = putStrLn $ "Unexpected server response:\n" ++ show ex
---
+main :: IO ()
+main =
+  catch
+    ( do
+        args <- getArgs
+        putStrLn ("ServerUrl: " ++ args !! 0 ++ "; PlayerKey: " ++ args !! 1)
+        request' <- parseRequest ("POST " ++ (args !! 0))
+        let request = setRequestBodyLBS (BLU.fromString (args !! 1)) request'
+        response <- httpLBS request
+        let statuscode = show (getResponseStatusCode response)
+        case statuscode of
+          "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
+          _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
+    )
+    handler
+  where
+    handler :: SomeException -> IO ()
+    handler ex = putStrLn $ "Unexpected server response:\n" ++ show ex
